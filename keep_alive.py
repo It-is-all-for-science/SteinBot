460925f72dfc930211119824ec86565f94407c40
It-is-all-for-science <nataliazeninskaya@mail.ru>
2025-07-03 20:45:01 +0300

    fix: correct keep_alive run function

---
+++ keep_alive.py
@@ -0,0 +1,16 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "SteinBot is alive!"
+
+@app.route('/health')
+def health():
+    return "OK"
+
+def run():
+    port = int(os.environ.get("PORT", 10000))  # Render всегда подставляет PORT
+    app.run(host="0.0.0.0", port=port)
